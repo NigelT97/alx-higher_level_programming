33cfcc290acc21f46787b6d5b1db763806f6e4cb
PStar <nigelmj97@gmail.com>
2023-06-14 22:39:41 +0200

    error fix 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    ndict -{}
+    ndict = {}
     for key, value in a_dictionary.items():
         ndict.update({key: (value * 2)})
     return ndict
